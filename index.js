53a5021f8df4a95a61d65ede5238438f8d34ffcc
Mateodans <mateodand@gmail.com>
2023-01-09 18:41:28 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const routerApi = require('./routes')
+
+const app = express()
+const port = 3000
+
+const { logError, errorHandler } = require('./middlewares/errorHandler')
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+routerApi(app)
+
+app.use(logError)
+app.use(errorHandler)
+
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
